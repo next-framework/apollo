a2ad9e3b12c5552fb8e9d0dc522db1d08d067073
ginozheng <ginozheng@tencent.com>
2019-11-04 18:26:00 +0800

    trying

--- router/handler.go
+++ router/handler.go
@@ -1,14 +1,29 @@
 package router
 
-import "github.com/next-frmework/apollo/config"
+import (
+	"github.com/next-frmework/apollo/config"
+	"net/http"
+)
 
 type Handler interface {
 	Handle(c *Context)
 }
 
-type HandlerMapping interface {
+type HandlerRouterMapping interface {
 	Add(router config.Router, handler Handler)
+	Resolve(r http.Request) (Handler, *Context, error)
 }
 
-type HandlerMappingAdapter struct {
+type DefaultHandlerRouterMapping struct {
+	// todo 构建一个用于path查找的语法树
+	Handlers map[string]Handler
+	Routers  map[string]*config.Router
+}
+
+func (hrm *DefaultHandlerRouterMapping) Add(router config.Router, handler Handler) {
+
+}
+
+func (hrm *DefaultHandlerRouterMapping) Resolve(r http.Request) (Handler, *Context, error) {
+	return nil, nil, nil
 }
